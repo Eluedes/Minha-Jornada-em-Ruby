06e0413b780e24f657f2f36bc26f4e37f7006ef8
Rafael Guedes <rafaelguedes@protonmail.com>
2024-11-11 22:13:51 -0300

    Iniciando em Ruby
    meus primeiros arquivos em ruby

---
+++ Curso 1/des1.rb
@@ -0,0 +1,10 @@
+print "Coloque o seu Nome: "
+    nome = gets.chomp
+
+print "Coloque o seu Sobrenome: "
+    sobrenome = gets.chomp
+
+print "Coloque sua idade: "
+    idade = gets.chomp
+
+puts " Nome Completo: #{nome} #{sobrenome} Idade: #{idade}"
