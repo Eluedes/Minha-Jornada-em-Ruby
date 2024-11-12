7dbf7627af4809f19fa301f8eae38634799937bd
Rafael Guedes <rafaelguedes@protonmail.com>
2024-11-11 22:17:59 -0300

    Add files via upload

---
+++ Curso 1/ex002.rb
@@ -0,0 +1,11 @@
+# Comando de saida, a onde ele faz a pergunta para o usuario
+print "Digita o seu nome: "
+
+# Comando de entrada que cria um input para o comando de saida
+nome = gets.chomp
+
+print "Digita o seu sobrenome: "
+sobrenome =  gets.chomp
+
+# Comando de saída final
+puts "Olá #{nome} #{sobrenome}"
