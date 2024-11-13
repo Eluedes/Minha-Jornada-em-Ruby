808d6657c74bbf877bc02e455753bbf6b26a606e
Rafael Guedes <rafaelguedes@protonmail.com>
2024-11-12 21:26:28 -0300

    Estrutura de controle usando IF, ELSE e ELSIF.

--- Curso 1/ex003.rb
+++ Curso 1/ex003.rb
@@ -1,1 +1,8 @@
 #Estrutura de controle usando IF, ELSE e ELSIF.
+dia = 'domingo'
+
+if dia == 'domingo' # == é uma comparação
+    almoco = 'especial'
+end
+
+puts "Hoje nosso almoço será #{almoco}"
