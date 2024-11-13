6f2698645864a06b9ebce900908adf82c66df38d
Eluedes <rafaelguedes@protonmail.com>
2024-11-12 21:39:07 -0300

    #Estrutura de controle usando IF, ELSE e ELSIF.

---
+++ Curso 1/ex003.rb
@@ -0,0 +1,20 @@
+<<<<<<< HEAD
+#Estrutura de controle usando IF, ELSE e ELSIF.
+dia = 'segunda'
+
+if dia == 'domingo' # == é uma comparação
+    almoco = 'especial'
+else 
+    almoco = 'normal'
+end
+
+=======
+#Estrutura de controle usando IF, ELSE e ELSIF.
+dia = 'segunda'
+
+if dia == 'domingo' # == é uma comparação
+    almoco = 'especial'
+end
+
+>>>>>>> fdf5a91a44cdec4be64e637c99984fe4f9a4a6a3
+puts "Hoje nosso almoço será #{almoco}"
