e64dbb006a1cdc262390686e4a70146b19ea8494
Rafael Guedes <rafaelguedes@protonmail.com>
2024-11-13 21:25:45 -0300

    Teste com CASE

--- Curso 1/exx005.rb
+++ Curso 1/exx005.rb
@@ -1,4 +1,20 @@
 puts 'Digite seu mês de nascimento: '
     mes = gets.chomp.to_i
 
-case
+    case mes
+    when 1..2 #.. intervalo
+        puts ' Você nasceu no primeiro trimestre do ano'
+
+    when 4..6
+        puts 'Você nasceu no primeiro semestre do ano'
+
+    when 7..9
+        puts ' você nasceu no terceiro trismestre'
+
+    when 10..12
+        puts 'Você é isso ai'
+
+    else
+        puts 'I valor digitado é invalido'
+
+        end
