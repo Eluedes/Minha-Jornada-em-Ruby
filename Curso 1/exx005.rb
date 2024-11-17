af5b4a66a720070688dde89811ecc203d95ed87f
Rafael Guedes <rafaelguedes@protonmail.com>
2024-11-17 20:23:07 -0300

    teste

--- Curso 1/exx005.rb
+++ Curso 1/exx005.rb
@@ -1,20 +1,15 @@
 puts 'Digite seu mês de nascimento: '
-    mes = gets.chomp.to_i
+mes = gets.chomp.to_i
 
-    case mes
-    when 1..2 #.. intervalo
-        puts ' Você nasceu no primeiro trimestre do ano'
-
-    when 4..6
-        puts 'Você nasceu no primeiro semestre do ano'
-
-    when 7..9
-        puts ' você nasceu no terceiro trismestre'
-
-    when 10..12
-        puts 'Você é isso ai'
-
-    else
-        puts 'I valor digitado é invalido'
-
-        end
+case mes
+when 1..3
+    puts 'Você nasceu no primeiro trimestre do ano'
+when 4..6
+    puts 'Você nasceu no segundo trimestre do ano'
+when 7..9
+    puts 'Você nasceu no terceiro trimestre do ano'
+when 10..12
+    puts 'Você nasceu no quarto trimestre do ano'
+else
+    puts 'O valor digitado é inválido'
+end
