33d0422bf4a2de30fc73dd2e9211d4e6c0d727a3
Rafael Guedes <rafaelguedes@protonmail.com>
2024-11-26 23:06:04 -0300

    Aprendendo sobre métados e Gem

---
+++ Módulo 2/Métodos e Gems/metado.rb
@@ -0,0 +1,20 @@
+def oi 
+  puts ' Você e dev!'
+end
+
+3.times do
+  oi
+end
+
+
+
+def nome (nome, sobrenome)
+  puts "Oi #{nome} #{sobrenome}, voê é um dev porra!!!"
+end
+
+nome = 'Rafael'
+sobrenome = 'Guedes'
+
+2.times do 
+  nome(nome, sobrenome)
+end
